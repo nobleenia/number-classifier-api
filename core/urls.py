5a583327b6b792731a5615a38c24dcb6843df1c2
Noble <neluwah@gmail.com>
2025-02-07 15:55:58 +0100

    Fixed API URL path issues

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from .views import classify_number, home
 
 urlpatterns = [
     path('', home, name="home"),
-    path('api/classify-number', classify_number, name="classify_number"),  # No trailing slash!
+    path('classify-number', classify_number, name="classify_number"),
 ]
