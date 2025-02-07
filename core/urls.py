f04dfa06e4107ca5692ca306d74392e9d74e67c2
Noble <neluwah@gmail.com>
2025-02-07 12:45:14 +0100

    Fixed deployment errors

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import classify_number, home
 
 urlpatterns = [
-    path('', home),
-    path('api/classify-number', classify_number),
+    path('',home, name="home"),
+    path('api/classify-number', classify_number, name="classify_number"),
 ]
